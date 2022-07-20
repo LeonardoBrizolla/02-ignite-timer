134cbf141c15a215f70ee215125889748c083e7f
Leonardo Brizolla <leonardo.brizolla@compasso.com.br>
2022-07-20 15:13:02 -0300

    Global Style

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -26,8 +26,8 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin-right: 4px;
 
-  background-color: ${props => props.theme.primary};
-  color: ${props => props.theme.secondary};
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
 
   /* ${props => {
     return css`

79f6275719a546c203a055245ebfe55d7c56b5e4
Leonardo Brizolla <leonardo.brizolla@compasso.com.br>
2022-08-22 13:59:13 -0300

    Component: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
 	return (
@@ -21,31 +21,41 @@ export function History() {
 							<td>Codar Histórico</td>
 							<td>20 min</td>
 							<td>Há 2 meses</td>
-							<td>Em andamento</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Codar Histórico</td>
 							<td>20 min</td>
 							<td>Há 2 meses</td>
-							<td>Em andamento</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Codar Histórico</td>
 							<td>20 min</td>
 							<td>Há 2 meses</td>
-							<td>Em andamento</td>
+							<td>
+								<Status statusColor="green">Concluído</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Codar Histórico</td>
 							<td>20 min</td>
 							<td>Há 2 meses</td>
-							<td>Em andamento</td>
+							<td>
+								<Status statusColor="red">Interrompido</Status>
+							</td>
 						</tr>
 						<tr>
 							<td>Codar Histórico</td>
 							<td>20 min</td>
 							<td>Há 2 meses</td>
-							<td>Em andamento</td>
+							<td>
+								<Status statusColor="yellow">Em andamento</Status>
+							</td>
 						</tr>
 					</tbody>
 				</table>
